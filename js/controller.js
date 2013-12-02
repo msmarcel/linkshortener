9e4df3e569f578be480f0e9862998b669287c0ae
T. Scott Barnes <barnes.t.scott@gmail.com>
2013-12-02 15:54:03 -0500

    Implemented checkboxes to disable individual services

--- js/controller.js
+++ js/controller.js
@@ -2,25 +2,49 @@
 
 function LinkShortenerCtrl($scope, $http) {
   $scope.linkURL = '';
-  $scope.shortlinks = [];
+  $scope.shortlinks = [{
+    'id': 'default',
+    'enabled': true,
+    'service': 'Original URL',
+    'type': null,
+    'api': null,
+    'shorturl': null
+  }, {
+    'id': 'isgd',
+    'enabled': true,
+    'service': 'is.gd',
+    'type': 'jsonp',
+    'api': 'http://is.gd/create.php?format=json&callback=JSON_CALLBACK&url=',
+    'shorturl': null
+  }];
+  $http.get('/services.json').success(function(data, status) {
+    for(var loop = 0; loop < data.length; loop++) {
+      $scope.shortlinks.push(data[loop]);
+    }
+  });
+
+  $scope.getLink = function(service) {
+    if(!service.type) {
+      service.shorturl = $scope.linkURL;
+    } else if(service.type == 'jsonp') {
+      $http.jsonp(service.api + encodeURI($scope.linkURL)).success(function(data, status) {
+        if(data) {
+          service.shorturl = data.shorturl;
+        }
+      });
+    } else if(service.type == 'get') {
+      $http.get(service.api + encodeURI($scope.linkURL)).success(function(data, status) {
+        if(data) {
+          service.shorturl = data.shorturl;
+        }
+      });
+    }
+  };
   
   $scope.submit = function() {
-    delete $scope.shortlinks;
-    $scope.shortlinks = [];
-    $scope.shortlinks.push({
-      'id': 'default',
-      'service': 'Original URL',
-      'shorturl': $scope.linkURL
-    });
-    $http.jsonp('http://is.gd/create.php?format=json&callback=JSON_CALLBACK&url=' + encodeURI($scope.linkURL)).success(function(data, status) {
-      if(data) {
-        $scope.shortlinks.push({
-          'id': 'isgd',
-          'service': 'is.gd',
-          'shorturl': data.shorturl
-        });
-      }
-    });
+    for(var loop = 0; loop < $scope.shortlinks.length; loop++) {
+      $scope.getLink($scope.shortlinks[loop]);
+    }
   };
 }
 
