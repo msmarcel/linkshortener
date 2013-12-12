44c5c2f75fef769083188f684aa133c2fd300a14
T. Scott Barnes <barnes.t.scott@gmail.com>
2013-12-12 08:14:08 -0500

    Started moving to use bitly for LinkedIn and Facebook

--- module/Application/src/Application/Controller/BitlyController.php
+++ module/Application/src/Application/Controller/BitlyController.php
@@ -23,32 +23,36 @@ class BitlyController extends AbstractServiceController
 
     protected function makeApiCall($url, $title, $description, $image)
     {
+        $config = $this->getServiceLocator()->get('Config');
         if (isset($this->session->accessToken)) {
-            $requestParams = array(
-                'access_token' => $this->session->accessToken->getAccessToken(),
-                'longUrl' => $url
+            $token = $this->session->accessToken->getAccessToken();
+        } else {
+            $token = $config['tokens'][$this->name];
+        }
+        $requestParams = array(
+            'access_token' => $token,
+            'longUrl' => $url
+        );
+        
+        $response = $this->client->get('https://api-ssl.bitly.com/v3/shorten', $requestParams);
+        $data = json_decode($response->getBody(), true);
+        
+        if (array_key_exists('url', $data['data'])) {
+            return array(
+                'success' => true,
+                'needauth' => false,
+                'origurl' => $url,
+                'shorturl' => $data['data']['url'],
+                'data' => $data
             );
-            
-            $response = $this->client->get('https://api-ssl.bitly.com/v3/shorten', $requestParams);
-            $data = json_decode($response->getBody(), true);
-            
-            if (array_key_exists('url', $data['data'])) {
-                return array(
-                    'success' => true,
-                    'needauth' => false,
-                    'origurl' => $url,
-                    'shorturl' => $data['data']['url'],
-                    'data' => $data
-                );
-            } else {
-                return array(
-                    'success' => false,
-                    'needauth' => false,
-                    'origurl' => $url,
-                    'shorturl' => $data['status_txt'],
-                    'data' => $data
-                );
-            }
+        } else {
+            return array(
+                'success' => false,
+                'needauth' => false,
+                'origurl' => $url,
+                'shorturl' => $data['status_txt'],
+                'data' => $data
+            );
         }
         
         return array(
